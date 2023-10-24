969dfce680a3acc4713352cae27594941ee33f52
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-10-24 12:09:12 +0200

    '==' to '===' | append announcement to outcomeDiv instead of containerDiv

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ function getComputerChoice() {
 }
 
 function checkWinner(playerChoice, computerChoice) {
-    if (playerChoice == "rock" && computerChoice == "scissors" || playerChoice == "paper" && computerChoice ==      "rock" || playerChoice == "scissors" && computerChoice == "paper") {
+    if (playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "paper" && computerChoice === "rock" || playerChoice === "scissors" && computerChoice === "paper") {
         return "player";
     }
-    else if (playerChoice == computerChoice) {
+    else if (playerChoice === computerChoice) {
         return "tie";
     }
     else {
@@ -24,40 +24,16 @@ function updateScore (playerScore, computerScore) {
     computerScoreSpan.textContent = `Computer Score: ${computerScore}`;
 }
 
-function announceWinner(playerScore, computerScore) {
-    const roundsToPlay = 5;
-    //get reference to container div to append win/loose message when the game ends
-    const container = document.querySelector("#container");
-    //create Element to display win/loose message
-    const announcement = document.createElement("h2");
-    //disable buttons as soon as the game ends
-    if (computerScore === roundsToPlay || playerScore === roundsToPlay) {
-        document.getElementById("1").disabled = true;
-        document.getElementById("2").disabled = true;
-        document.getElementById("3").disabled = true; 
-    }
-    if (computerScore === roundsToPlay) {
-        announcement.textContent = "You lost!"
-        announcement.style.color = "red";
-        container.appendChild(announcement);
-    }
-    else if (playerScore === roundsToPlay) {
-        announcement.textContent = "You won!";
-        announcement.style.color = "lightgreen";
-        container.appendChild(announcement);
-    }
-}
-
 //plays one round of rock paper scissors and updates scores
 function play(playerChoice, computerChoice) {
     //declare variable to get result from checkWinner function to use in if statements to display the rounds result message in the outcome paragraph
     const result = checkWinner(playerChoice, computerChoice);
-    if (result == "player") {
+    if (result === "player") {
         console.log(`You win! ${playerChoice} beats ${computerChoice}`);
         outcome.textContent = `You win! ${playerChoice} beats ${computerChoice}`;     
         playerScore++;
     }
-    else if (result == "tie") {
+    else if (result === "tie") {
         console.log("It's a tie!");
         outcome.textContent = "It's a tie!";
     }
@@ -65,6 +41,31 @@ function play(playerChoice, computerChoice) {
         console.log(`You loose! ${computerChoice} beats ${playerChoice}`);
         outcome.textContent = `You loose! ${computerChoice} beats ${playerChoice}`;
         computerScore++;
+    }
+}
+
+function announceWinner(playerScore, computerScore) {
+    const roundsToPlay = 5;
+    //get reference to outcomeDiv to append a win/loose message
+    const outcomeDiv = document.querySelector("#outcome");
+    //create Element to display win/loose message
+    const announcement = document.createElement("h1");
+    //disable buttons as soon as the game ends
+    if (computerScore === roundsToPlay || playerScore === roundsToPlay) {
+        document.getElementById("1").disabled = true;
+        document.getElementById("2").disabled = true;
+        document.getElementById("3").disabled = true; 
+        outcome.textContent = "Game over";
+    }
+    if (computerScore === roundsToPlay) {
+        announcement.textContent = "You lost!"
+        announcement.style.color = "#E74C3C";
+        outcomeDiv.appendChild(announcement);
+    }
+    else if (playerScore === roundsToPlay) {
+        announcement.textContent = "You won!";
+        announcement.style.color = "#2ECC71";
+        outcomeDiv.appendChild(announcement);
     }
 }
 
@@ -80,9 +81,10 @@ const containerDiv = document.querySelector("#container");
 //create paragraph for the play function to display the outcome of a around
 const outcome = document.createElement("p");
 outcome.textContent = "";
+outcome.style.color = "#1B4F72";
 containerDiv.appendChild(outcome);
 
-//get references to rock, paper, scissors -buttons
+//get references to rock, paper, scissors -buttons 
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
@@ -111,4 +113,4 @@ scissorsBtn.addEventListener("click", () => {
     announceWinner(playerScore, computerScore);
 })
 
-//todo: add reset button    add some styling
+//todo: add reset button   
