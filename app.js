ca3098ab1975924c5fe5288c5869e088337c8785
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-10-24 11:41:31 +0200

    deleted some unnecessary lines | added comments | structured the code

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-//array of possible choices other functions can use
-const choices = ["rock", "paper", "scissors"];
-
 function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
@@ -18,49 +16,41 @@ function checkWinner(playerChoice, computerChoice) {
     }
 }
 
-let computerScore = 0;
-let playerScore = 0;
-
-const scoreDiv = document.querySelector("#score");
-const playerScoreSpan = document.querySelector("#playerScoreSpan");
-const computerScoreSpan = document.querySelector("#computerScoreSpan");
-
 function updateScore (playerScore, computerScore) {
+    //get references to player and computer score spans to update scores
+    const playerScoreSpan = document.querySelector("#playerScoreSpan");
+    const computerScoreSpan = document.querySelector("#computerScoreSpan");
     playerScoreSpan.textContent =  `Player Score: ${playerScore}`;
     computerScoreSpan.textContent = `Computer Score: ${computerScore}`;
 }
 
 function announceWinner(playerScore, computerScore) {
+    const roundsToPlay = 5;
+    //get reference to container div to append win/loose message when the game ends
     const container = document.querySelector("#container");
+    //create Element to display win/loose message
     const announcement = document.createElement("h2");
-    //disable buttons as soon as either the player or the computer reaches 5 points
-    if (computerScore === 5 || playerScore === 5) {
+    //disable buttons as soon as the game ends
+    if (computerScore === roundsToPlay || playerScore === roundsToPlay) {
         document.getElementById("1").disabled = true;
         document.getElementById("2").disabled = true;
         document.getElementById("3").disabled = true; 
     }
-    if (computerScore === 5) {
+    if (computerScore === roundsToPlay) {
         announcement.textContent = "You lost!"
         announcement.style.color = "red";
         container.appendChild(announcement);
     }
-    else if (playerScore === 5) {
+    else if (playerScore === roundsToPlay) {
         announcement.textContent = "You won!";
         announcement.style.color = "lightgreen";
         container.appendChild(announcement);
     }
 }
 
-const containerDiv = document.querySelector("#container");
-
-const outcome = document.createElement("p");
-outcome.textContent = "";
-containerDiv.appendChild(outcome);
-
-
-//plays one round of rock paper scissors 
+//plays one round of rock paper scissors and updates scores
 function play(playerChoice, computerChoice) {
-    //declare variable to get result from checkWinner function to use in if statements
+    //declare variable to get result from checkWinner function to use in if statements to display the rounds result message in the outcome paragraph
     const result = checkWinner(playerChoice, computerChoice);
     if (result == "player") {
         console.log(`You win! ${playerChoice} beats ${computerChoice}`);
@@ -78,8 +68,21 @@ function play(playerChoice, computerChoice) {
     }
 }
 
+//declare global variables to keep track of scores and for other functions to use
+let computerScore = 0;
+let playerScore = 0;
+
+
 //UI
 
+//get reference to container element to appends a paragraph that displays the outcome when a round is played
+const containerDiv = document.querySelector("#container");
+//create paragraph for the play function to display the outcome of a around
+const outcome = document.createElement("p");
+outcome.textContent = "";
+containerDiv.appendChild(outcome);
+
+//get references to rock, paper, scissors -buttons
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
