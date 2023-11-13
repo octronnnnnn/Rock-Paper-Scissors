cfc9501eeb5c4d352630d81db58192ff723ac61c
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-11-13 14:14:28 +0100

    new reset function without reloading the page | done

--- app.js
+++ app.js
@@ -1,7 +1,77 @@
+// UI
+// get references to elements
+const outcomeDiv = document.querySelector("#outcome");
+const btnContainer = document.querySelector("#container");
+const winnerAnnouncement = document.querySelector("#winnerAnnouncement");
+const playerChoiceDisplay = document.querySelector("#playerChoiceDisplay");
+const computerChoiceDisplay = document.querySelector("#computerChoiceDisplay");
+
+//create element to display the outcome of a around in the outcomeDiv
+const outcome = document.createElement("p");
+outcome.textContent = "placeholder";
+outcome.classList.add("hide");
+outcomeDiv.appendChild(outcome);
+
+// slide-fade-in animation
+function setAnimationClass () {
+    outcome.classList.add("fade");
+    playerChoiceDisplay.classList.add("fade");
+    computerChoiceDisplay.classList.add("fade");
+}
+
+function animate() {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            setTimeout(setAnimationClass, 0);
+            outcome.classList.remove("fade");
+            playerChoiceDisplay.classList.remove("fade");
+            computerChoiceDisplay.classList.remove("fade");
+        })
+    })
+}
+
+
+// game
+let computerScore = 0;
+let playerScore = 0;
+const roundsToPlay = 5;
+
+function game () {
+    animate();
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const playerChoice = button.id;
+        const computerChoice = getComputerChoice();
+        play(playerChoice, computerChoice)
+        updateVsDisplay(playerChoice, computerChoice);
+        updateScore(playerScore, computerScore);
+        announceWinner(playerScore, computerScore);
+    })
+})
+}
+
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
+}
+
+//plays one round of rock paper scissors and updates scores
+function play(playerChoice, computerChoice) {
+    const result = checkWinner(playerChoice, computerChoice);
+    if (result === "player") {
+        outcome.textContent = `You win! ${playerChoice} beats ${computerChoice}`;     
+        playerScore++;
+    }
+    else if (result === "tie") {
+        outcome.textContent = "It's a tie!";
+    }
+    else {
+        outcome.textContent = `You loose! ${computerChoice} beats ${playerChoice}`;
+        computerScore++;
+    }
 }
 
 function checkWinner(playerChoice, computerChoice) {
@@ -16,51 +86,43 @@ function checkWinner(playerChoice, computerChoice) {
     }
 }
 
+function updateVsDisplay (playerChoice, computerChoice) {
+    let rockImg = "./icons/RockPixelArt.png";
+    let paperImg = "./icons/PaperPixelArt.png";
+    let scissorsImg = "./icons/scissors.png";
+
+    if (playerChoice === "rock") {
+        playerChoiceDisplay.src = rockImg;
+    }
+    else if (playerChoice === "paper") {
+        playerChoiceDisplay.src = paperImg;
+    }
+    else {
+        playerChoiceDisplay.src = scissorsImg;
+    }
+    if (computerChoice === "rock") {
+        computerChoiceDisplay.src = rockImg;
+    }
+    else if (computerChoice === "paper") {
+        computerChoiceDisplay.src = paperImg;
+    }
+    else {
+        computerChoiceDisplay.src = scissorsImg;
+    }
+}
+
 function updateScore (playerScore, computerScore) {
-    //get references to player and computer score spans to update scores
     const playerScoreSpan = document.querySelector("#playerScoreSpan");
     const computerScoreSpan = document.querySelector("#computerScoreSpan");
     playerScoreSpan.textContent = `Player: ${playerScore}`;
     computerScoreSpan.textContent = `Computer: ${computerScore}`;
 }
 
-//plays one round of rock paper scissors and updates scores
-function play(playerChoice, computerChoice) {
-    //declare variable to get result from checkWinner function to use in if statements to display the rounds result message in the outcome paragraph
-    const result = checkWinner(playerChoice, computerChoice);
-    if (result === "player") {
-        console.log(`You win! ${playerChoice} beats ${computerChoice}`);
-        outcome.textContent = `You win! ${playerChoice} beats ${computerChoice}`;     
-        playerScore++;
-    }
-    else if (result === "tie") {
-        console.log("It's a tie!");
-        outcome.textContent = "It's a tie!";
-    }
-    else {
-        console.log(`You loose! ${computerChoice} beats ${playerChoice}`);
-        outcome.textContent = `You loose! ${computerChoice} beats ${playerChoice}`;
-        computerScore++;
-    }
-}
-
-function reset () {
-    //create reset button
-    const resetBtn = document.createElement("button");
-    resetBtn.textContent = "Play again";
-    resetBtn.classList.add("resetBtn");
-    btnContainer.appendChild(resetBtn);
-    resetBtn.addEventListener("click", () => {
-        location.reload();
-    })
-}
-
 function announceWinner(playerScore, computerScore) {
-    const roundsToPlay = 5;
-    //remove buttons as soon as the game ends | empty choice dispaly | add replay button | change vsText to dispay winner announcement
+    // remove buttons as soon as the game ends | add replay button 
     if (computerScore === roundsToPlay || playerScore === roundsToPlay) {
         removeButtons();
-        reset();
+        resetGame();
         outcome.textContent = "Game Over!";
         winnerAnnouncement.classList.add("fade");
     }
@@ -83,95 +145,49 @@ function removeButtons() {
     btnContainer.removeChild(scissors);
 }
 
-function updateVsDisplay (playerChoice, computerChoice) {
-    let rock = "./icons/RockPixelArt.png"
-    let paper = "./icons/PaperPixelArt.png"
-    let scissors = "./icons/scissors.png"
-
-    if (playerChoice === "rock") {
-        playerChoiceDisplay.src = rock;
-    }
-    else if(playerChoice === "paper") {
-        playerChoiceDisplay.src = paper;
-    }
-    else {
-        playerChoiceDisplay.src = scissors;
-    }
-    if (computerChoice === "rock") {
-        computerChoiceDisplay.src = rock;
-    }
-    else if(computerChoice === "paper") {
-        computerChoiceDisplay.src = paper;
-    }
-    else {
-        computerChoiceDisplay.src = scissors;
-    }
-    playerChoiceDisplay.style.width = "60px";
-    playerChoiceDisplay.style.height = "60px";
-    computerChoiceDisplay.style.width = "60px";
-    computerChoiceDisplay.style.height = "60px";
-}
-
-function game () {
-    //get reference to all buttons 
-    const buttons = document.querySelectorAll("button");
-    //give each button an eventListener
-    buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const playerChoice = button.id;
-        const computerChoice = getComputerChoice();
-        play(playerChoice, computerChoice)
-        updateVsDisplay(playerChoice, computerChoice);
-        updateScore(playerScore, computerScore);
-        announceWinner(playerScore, computerScore);
+function resetGame() {
+    //create replay button
+    const resetBtn = document.createElement("button");
+    resetBtn.textContent = "Play again";
+    resetBtn.classList.add("resetBtn");
+    btnContainer.appendChild(resetBtn);
+    //reset the game
+    resetBtn.addEventListener("click", () => {
+        playerScore = 0;
+        computerScore = 0;
+        resetAnimationClasses();
+        createNewButtons();
+        btnContainer.removeChild(resetBtn);
+        updateScore(playerScore, computerScore);       
+        game();
     })
-})
 }
 
-//declare global variables to keep track of scores and for other functions to use
-let computerScore = 0;
-let playerScore = 0;
-
-
-//UI
-
-//animation
-function onTick () {
-    outcome.classList.add("fade");
-    playerChoiceDisplay.classList.add("fade");
-    computerChoiceDisplay.classList.add("fade");
+function resetAnimationClasses() {
+    playerChoiceDisplay.classList.remove("fade");
+    computerChoiceDisplay.classList.remove("fade");
+    winnerAnnouncement.classList.remove("fade");
+    outcome.classList.add("hide");
+    outcome.classList.remove("fade");    
 }
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        let timer = setTimeout(onTick, 0);
-        outcome.classList.remove("fade");
-        playerChoiceDisplay.classList.remove("fade");
-        computerChoiceDisplay.classList.remove("fade");
-    })
-})
-
-//get reference to outcomeDiv to append outcome messages
-const outcomeDiv = document.querySelector("#outcome");
-
-//get reference to button container to append reset button after game ends
-const btnContainer = document.querySelector("#container");
-
-//get reference to winnerAnnouncement text
-const winnerAnnouncement = document.querySelector("#winnerAnnouncement");
-
-//get reference to displayed player/pc choices
-const playerChoiceDisplay = document.querySelector("#playerChoiceDisplay");
-const computerChoiceDisplay = document.querySelector("#computerChoiceDisplay");
-playerChoiceDisplay.classList.add("hideLeft");
-computerChoiceDisplay.classList.add("hideRight");
-
-//create paragraph for the play function to display the outcome of a around
-const outcome = document.createElement("p");
-outcome.textContent = "placeholder";
-outcome.classList.add("hide");
-outcomeDiv.appendChild(outcome);
+function createNewButtons() {
+    let rock = document.createElement("button");
+    let paper = document.createElement("button");
+    let scissors = document.createElement("button");
+    rock.classList.add("selectionBtn");
+    paper.classList.add("selectionBtn");
+    scissors.classList.add("selectionBtn");
+    rock.id = "rock";
+    paper.id = "paper";
+    scissors.id = "scissors";
+    rock.innerHTML = '<img src="./icons/RockPixelArt.png"/>';
+    paper.innerHTML = '<img src="./icons/PaperPixelArt.png"/>';
+    scissors.innerHTML = '<img src="./icons/scissors.png"/>';
+    btnContainer.appendChild(rock);
+    btnContainer.appendChild(paper);
+    btnContainer.appendChild(scissors);
+}
 
 
 game ();
